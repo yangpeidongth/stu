58c3d28dfa3c1a96a56b53f9f1a3b9f653a65c3d
yangpeidongth <413958624@qq.com>
2018-11-09 14:35:31 +0800

    test

---
+++ Euraka/src/main/java/com/tedu/RunEuraka.java
@@ -0,0 +1,14 @@
+package com.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class RunEuraka {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunEuraka.class, args);
+	}
+
+}
